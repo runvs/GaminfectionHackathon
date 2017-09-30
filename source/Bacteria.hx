ba71b5ce7edeef95303cfab7d215c141c28ea663
Simon Weis <laguna.ws@gmail.com>
2017-09-30 14:14:53 +0200

    getting bacteria movement right

--- source/Bacteria.hx
+++ source/Bacteria.hx
@@ -2,6 +2,7 @@ package;
 
 import flixel.FlxG;
 import flixel.FlxSprite;
+import flixel.math.FlxPoint;
 import flixel.system.FlxAssets.FlxGraphicAsset;
 import flixel.util.FlxColor;
 
@@ -11,53 +12,75 @@ import flixel.util.FlxColor;
  */
 class Bacteria extends FlxSprite
 {
+	
+	// encoded in phi, r;
+	private var age : Float = 0;
+	
 
 	public function new(?X:Float=0, ?Y:Float=0) 
 	{
 		super(X, Y);
 		makeGraphic(24, 24, FlxColor.WHITE);
 		this.drag.set(GP.BaceteriaMoveDrag, GP.BaceteriaMoveDrag);
-		
+		this.maxVelocity.set(GP.BacteriaMoveMaxSpeed, GP.BacteriaMoveMaxSpeed);
+
 	}
+	
+	
 	
 	override public function update(elapsed:Float):Void 
 	{
 		super.update(elapsed);
+		age += elapsed;
 		
 		this.acceleration.set();
 		DoMovement();
-		
+		AddrandomMovement(elapsed);
 		
 		
 		ClampPosition();
 		
 		
+		
+		
+	}
+	
+	function AddrandomMovement(elapsed : Float) 
+	{
+		
+		var val : Float = Math.pow((Math.sin(age)*Math.sin(2*age)*Math.sin(0.66*age)),2);
+	
+		var rvx : Float = Math.cos(age) * GP.BacteriaRandomMoveStrength * val;
+		var rvy : Float = Math.sin(age) * GP.BacteriaRandomMoveStrength * val;
+		
+		//velocity.set(velocity.x + rvx, velocity.y + rvy);
+		this.setPosition(x + rvx * elapsed, y + rvy * elapsed);
 	}
 	
 	function ClampPosition() 
 	{
-		if (x < GP.WorldBounds)
+		if (x < GP.WorldPadding)
 		{
-			x = GP.WorldBounds; 
+			x = GP.WorldPadding; 
 			this.velocity.x = 0;
 			this.acceleration.x = 0;
 		}
-		if (x > FlxG.width - GP.WorldBounds - this.width)
+		if (x > FlxG.width - GP.WorldPadding - this.width)
 		{
-			x = FlxG.width - GP.WorldBounds - this.width;
+			x = FlxG.width - GP.WorldPadding - this.width;
 			this.velocity.x = 0;
 			this.acceleration.x = 0;
 		}
 		
-		if (y < GP.WorldBounds)
+		if (y < GP.WorldPadding)
 		{
-			y = GP.WorldBounds;
+			y = GP.WorldPadding;
 			this.velocity.y = 0;
 			this.acceleration.y = 0;
 		}
-		if (y > FlxG.height - GP.WorldBounds - this.height)
+		if (y > FlxG.height - GP.WorldPadding - this.height)
 		{
-			y = FlxG.height - GP.WorldBounds - this.height;
+			y = FlxG.height - GP.WorldPadding - this.height;
 			this.velocity.y = 0;
 			this.acceleration.y = 0;
 		}
--- source/GP.hx
+++ source/GP.hx
@@ -7,8 +7,14 @@ package;
 class GP
 {
 
-	public static var WorldBounds (default, null) : Float  = 5;
+	public static var WorldPadding (default, null) : Float  = 5;
+	
 	static public var BacteriaMoveStrength (default, null) : Float = 400;
 	static public var BaceteriaMoveDrag (default, null) : Float = 300;
+	static public var BacteriaMoveMaxSpeed (default, null) : Float = 300;
+	
+	static public var BacteriaRandomMoveRotationSpeed (default, null) : Float = 1.17777777;
+	static public var BacteriaRandomMoveStrength (default, null) : Float = 65;
+	
 	
 }
